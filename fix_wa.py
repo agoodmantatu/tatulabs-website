fd82b3a4ca42b72522bc7c8806648361ff7b2153
agoodmantatu <agoodmantatu@gmail.com>
2026-06-26 23:38:02 +0530

    Fix: WhatsApp number updated

---
+++ fix_wa.py
@@ -0,0 +1,4 @@
+﻿src = open("E:/TatuLabs/index.html", encoding="utf-8").read()
+src = src.replace("91XXXXXXXXXX", "919566698821")
+open("E:/TatuLabs/index.html", "w", encoding="utf-8").write(src)
+print("WhatsApp number updated")
